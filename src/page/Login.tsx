9697932ef4782cc6fb59d83616995c379b5bfd7f
NuttaScholar <nutta766@gmail.com>
2025-05-23 21:58:11 +0700

    Create Login Page

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,10 +1,44 @@
+import { useNavigate } from "react-router-dom";
 import AppBar_c, { pageApp_e } from "../component/Organisms/AppBar_c";
+import {
+  SignInPage,
+  type AuthProvider,
+  type AuthResponse,
+} from "@toolpad/core/SignInPage";
+
+const providers = [
+  { id: "credentials", name: "Email and password" },
+];
 
 const Page_Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const signIn: (
+    provider: AuthProvider,
+    formData?: FormData
+  ) => Promise<AuthResponse> | void = async (provider, formData) => {
+    const promise = new Promise<AuthResponse>((resolve) => {
+      console.log("provider", provider);
+      console.log(
+        `formData: ${formData?.get("email")}, ${formData?.get("password")}`
+      );
+
+      navigate("/access")
+      resolve({ type: "CredentialsSignin", error: "Invalid credentials." });
+    });
+
+    return promise;
+  };
   return (
     <>
-      <AppBar_c role="admin" page={pageApp_e.logout}/>
-      <h1>Welcome to the Login Page</h1>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{
+          emailField: { autoFocus: false },
+          form: { noValidate: true },
+        }}
+      />
     </>
   );
 };
