b845bd7b922db872b5c06bd806a442b82999ed39
NuttaScholar <nutta766@gmail.com>
2025-09-09 09:00:19 +0700

    UpgrageComponent: Add props MinWidth and Direction on Field.tsx

--- src/component/Atoms/Field.tsx
+++ src/component/Atoms/Field.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import { SxProps, Theme } from "@mui/material";
 import { css_alignItem_t, css_display_t } from "../../type";
+import * as CSS from 'csstype';
+
 //*********************************************
 // Style
 //*********************************************
 const field: SxProps<Theme> = {
-  minWidth: "300px",
   width: "100%",
   padding: "8px 16px",
   borderRadius: "8px",
@@ -22,7 +23,9 @@ interface myProps {
   color?: string;
   hide?: boolean;
   display?: css_display_t;
+  minWidth?: string;
   maxWidth?: string;
+  direction?: CSS.Property.FlexDirection;
 }
 //*************************************************
 // Function
@@ -32,11 +35,14 @@ const Field: React.FC<myProps> = (props) => {
     <Box
       sx={{
         ...field,
+
         display: props.display||"flex",
         padding: props.hide ? "0" : "8px 16px",
         bgcolor: props.color || "secondary.main",
         alignItems: props.alignItem,
+        minWidth: props.minWidth || "300px",
         maxWidth: props.maxWidth || "480px",
+        flexDirection: props.direction
       }}
     >
       {props.children}
