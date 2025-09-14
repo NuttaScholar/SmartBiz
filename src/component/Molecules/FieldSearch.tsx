229179d345e1edf63117221de3c4cb2abace78e4
NuttaScholar <nutta766@gmail.com>
2025-09-14 13:10:06 +0700

    Maintenance FieldSearch.tsx:  Due to changing of Field.tsx

--- src/component/Molecules/FieldSearch.tsx
+++ src/component/Molecules/FieldSearch.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import Field from "../Atoms/Field";
 import { css_display_t } from "../../type";
+import Box from "@mui/material/Box";
 
 //*********************************************
 // Style
@@ -30,29 +31,44 @@ interface myProps {
 //*********************************************
 const FieldSearch: React.FC<myProps> = (props) => {
   return (
-    <Field hide={props.fieldHide} display={props.display} maxWidth={props.maxWidth}>
-      <TextField
-        label={props.label}
-        placeholder={props.placeholder}
-        type="text"
-        size="small"
-        value={props.value}
-        onChange={props.onChange}
-        sx={{ width: "100%" }}
-        slotProps={{
-          input: {
-            sx: { bgcolor: "white" },
-          },
-        }}
-      />
-      <IconButton
-        aria-label="Search"
-        onClick={() => {
-          props.onSubmit?.(props.value||"");
+    <Field
+      hide={props.fieldHide}
+      display={props.display}
+      maxWidth={props.maxWidth}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          borderRadius: "8px",
+          width: "100%",
+          gap: "8px",
+          boxSizing: "border-box",
+          backgroundColor: "secondary.main",
         }}
       >
-        <SearchIcon />
-      </IconButton>
+        <TextField
+          label={props.label}
+          placeholder={props.placeholder}
+          type="text"
+          size="small"
+          value={props.value}
+          onChange={props.onChange}
+          sx={{ width: "100%" }}
+          slotProps={{
+            input: {
+              sx: { bgcolor: "white" },
+            },
+          }}
+        />
+        <IconButton
+          aria-label="Search"
+          onClick={() => {
+            props.onSubmit?.(props.value || "");
+          }}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Box>
     </Field>
   );
 };
