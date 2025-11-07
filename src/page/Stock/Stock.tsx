f018aff0883579956d0a506ca02c934054d5974b
NuttaScholar <nutta766@gmail.com>
2025-11-08 01:11:11 +0700

    Refactor: Clean up Page_Stock component structure and add DialogHistory.

--- src/page/Stock/Stock.tsx
+++ src/page/Stock/Stock.tsx
@@ -12,30 +12,37 @@ import SpeedDial_Stock from "./component/SpeedDialStock";
 import DialogFormProduct from "./component/DialogFormProduct";
 import StockListHeader from "./component/StockListHeader";
 import { productInfo_t } from "../../API/StockService/type";
+import DialogHistory from "./component/DialogHistory";
 
 const Page_Stock: React.FC = () => {
   const [state, setState] = React.useState<stock_t>(StockDefaultState);
   const onClickList = (edit: boolean, value: productInfo_t) => {
-    if(edit){
-      setState({...state, dialogOpen: stockDialog_e.productForm, productForm: value})
+    if (edit) {
+      setState({
+        ...state,
+        dialogOpen: stockDialog_e.productForm,
+        productForm: value,
+      });
+    } else {
+      setState({
+        ...state,
+        dialogOpen: stockDialog_e.logs,
+        productForm: value,
+      });
     }
-  }          
+  };
   return (
     <StockContext.Provider value={{ state, setState }}>
       <AppBar_c>
         <StockStatus />
         <StockListHeader>
-          <StockList
-            variant="editable"
-            onClick={onClickList}
-          />
+          <StockList variant="editable" onClick={onClickList} />
         </StockListHeader>
 
         <SpeedDial_Stock />
       </AppBar_c>
-      <DialogFormProduct
-        onClose={() => setState({ ...state, dialogOpen: stockDialog_e.none })}
-      />
+      <DialogHistory />
+      <DialogFormProduct />
     </StockContext.Provider>
   );
 };
