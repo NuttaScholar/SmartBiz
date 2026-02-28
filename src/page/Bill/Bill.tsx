0aaf63ce75faa6c0930659d0998b65f3bba9bec0
NuttaScholar <nutta766@gmail.com>
2026-02-28 09:41:04 +0700

    Refactor Page_Bill component: Remove unused imports and enhance state management for order details

--- src/page/Bill/Bill.tsx
+++ src/page/Bill/Bill.tsx
@@ -1,38 +1,41 @@
 import React from "react";
 import AppBar_c from "../../component/Organisms/AppBar_c";
-import StatusPanel, {
-  listStatus_t,
-} from "../../component/Organisms/StatusPanal";
 import {
   BillContext,
   BillDefaultState,
+  billDialog_e,
   billState_t,
 } from "./context/BillContext";
-import OrderStatus from "./component/OrderStatus";
 import OrderListHeader from "./component/OrderListHeader";
 import OrderList from "./component/OrderList";
 import SpeedDial_Bill from "./component/SpeedDialBill";
-//*************************************************
-// Data Set
-//*************************************************
-const list: listStatus_t = [
-  { label: "Paid", id: "paid", color_value: "success" },
-  { label: "Unpaid", id: "unpaid", color_value: "error" },
-  { label: "Pending", id: "pending", color_value: "warning" },
-];
+import DialogOrderDetail from "./component/DialogOrderDetail";
+import { orderInfo_t } from "../../API/BillService/type";
+import DialogOrderForm from "./component/DialogOrderForm";
 //*************************************************
 // Function
 //*************************************************
 const Page_Bill: React.FC = () => {
-  const [state, setState] = React.useState<billState_t>(BillDefaultState);
+  const [state, setState] = React.useState<billState_t>({
+    ...BillDefaultState,
+    containerRef: React.useRef<HTMLDivElement>(null),
+  });
+  const [detail, setDetail] = React.useState<orderInfo_t>();
+
+  const onClickOrderList = (value: orderInfo_t) => {
+    setState({ ...state, dialogOpen: billDialog_e.detail });
+    setDetail(value);
+  };
   return (
     <BillContext.Provider value={{ state, setState }}>
       <AppBar_c>
         <OrderListHeader>
-          <OrderList/>
+          <OrderList onClick={onClickOrderList} />
         </OrderListHeader>
         <SpeedDial_Bill />
       </AppBar_c>
+      <DialogOrderDetail value={detail} />
+      <DialogOrderForm/>
     </BillContext.Provider>
   );
 };
