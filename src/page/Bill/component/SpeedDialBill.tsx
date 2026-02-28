acf1139cbad7059ccdd5f9b3f848e96e5165d26e
NuttaScholar <nutta766@gmail.com>
2026-02-28 09:38:55 +0700

    Refactor SpeedDial_Bill component: Remove unused imports and streamline state management

--- src/page/Bill/component/SpeedDialBill.tsx
+++ src/page/Bill/component/SpeedDialBill.tsx
@@ -3,10 +3,7 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import MySpeedDial from "../../../component/Molecules/MySpeedDial";
 import { menuList_t } from "../../../component/Molecules/ButtonOption";
-import { GoToTop } from "../../../function/Window";
 import React from "react";
-import UploadIcon from "@mui/icons-material/Upload";
-import DownloadIcon from "@mui/icons-material/Download";
 import { billDialog_e } from "../context/BillContext";
 import { useBillContext } from "../hooks/useBillContex";
 
@@ -30,16 +27,16 @@ const MenuList: menuList_t[] = [
 //*********************************************
 const SpeedDial_Bill: React.FC = () => {
   // Hook ************************************
-  const { state } = useBillContext();
+  const { state, setState } = useBillContext();
   // Local function **************************
   const speedDialHandler = (index: number) => {
     console.log(`SpeedDial: ${index}`);
     switch (index) {
       case 0:
-        
+        setState({ ...state, dialogOpen: billDialog_e.form });
         break;
-      case 3:
-        GoToTop();
+      case 1:
+        setState({...state, triger_gotoTop: (state.triger_gotoTop || 0) + 1});
         break;
     }
   };
