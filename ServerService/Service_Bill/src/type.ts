304d2fc6bf0b0dfd4444c295692e6c85e4453bdd
NuttaScholar <nutta766@gmail.com>
2026-05-23 16:21:28 +0700

    feat: add orderInfo_t and productInfo_t types to enhance type definitions

--- ServerService/Service_Bill/src/type.ts
+++ ServerService/Service_Bill/src/type.ts
@@ -1,4 +1,25 @@
-import { role_e } from "./utils/enum";
+
+import { OrderStatus, productType_e, role_e, stockStatus_e } from "./utils/enum";
+
+export type productInfo_t = {
+  id: string;
+  name: string;
+  img: string;
+  price: number;
+  amount: number;
+  total: number;
+  percentDiscount?: number;
+  priceAfterDiscount?: number;
+}
+
+export type orderInfo_t = {
+    orderID: string;
+    customerID: string;
+    date: Date;
+    totalAmount: number;
+    items: productInfo_t[];
+    status: OrderStatus;
+}
 
 export type tokenPackage_t = {
   username: string;
